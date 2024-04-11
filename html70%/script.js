1cac57f2e3681ad45e15b69baefac96ef777bc5e
jpb2511 <166556980+jpb2511@users.noreply.github.com>
2024-04-11 15:42:59 +0100

    Add files via upload

---
+++ html70%/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('myButton');
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
